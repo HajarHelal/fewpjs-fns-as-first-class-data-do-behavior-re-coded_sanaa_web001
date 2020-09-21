afdfb88b5490f0bb4a58c7150947b5043c7a0c13
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 20:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 function greet(timeString){
     const hour = parseInt(timeString, 10);
-  if ( hour < 12) return "Good Afternoon"
+  if ( hour < 12) return "Good Morning"
   if ( hour > 17) return "Good Evening"
-  return "Good Morning"
+  return "Good Afternoon"
 }
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
